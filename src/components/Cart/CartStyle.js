d7ad02d8f63f0bb2abad21c3daa09c9c0d290f5c
ArgLD <argleydsondev@gmail.com>
2021-09-28 09:01:45 -0300

    troquei o nome da pasta assets pra images

--- src/components/Cart/CartStyle.js
+++ src/components/Cart/CartStyle.js
@@ -6,7 +6,7 @@ export const CartContainer = styled.div`
     justify-content: space-between;
     height: 70vh;
     background-image: url("https://mir-s3-cdn-cf.behance.net/project_modules/fs/3609e971558965.5bc9241b50ac9.jpg");
-    /* background-image: url("/src/assets/swordBackground.jpg");  */
+    /* background-image: url("/src/images/swordBackground.jpg");  */
     background-position: center;   
     background-size: cover;
 `
