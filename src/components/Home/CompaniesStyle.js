0f2038f09799631575b63419f69810b0cc668430
ArgLD <argleydsondev@gmail.com>
2021-09-29 00:14:34 -0300

    adicionei imagens e mudei o layout

---
+++ src/components/Home/CompaniesStyle.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+export const CompaniesContainer = styled.div`
+    background-color: yellow;
+    height: 40vh;
+    width: 100vw;
+    color: blue;
+    padding: 8px;
+`
---
+++ src/components/Home/DepoimentosStyle.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+export const DepoimentosContainer = styled.div`
+    background-color: blue;
+    height: 80vh;
+    width: 100vw;
+    /* color: blue; */
+    padding: 8px;
+`
